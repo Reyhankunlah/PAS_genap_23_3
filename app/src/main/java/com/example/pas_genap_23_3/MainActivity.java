5f4fcd7b4c037cfc05d95ff47856860675cc6915
Muhammad Al Faiz <reyhankunlah@gmail.com>
2025-05-22 07:36:54 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pas_genap_23_3/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.pas_genap_23_3;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+    }
+}
